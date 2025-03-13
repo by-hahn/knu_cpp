6c020fbf335152d34df047bd387e59cc1264bb67
by-hahn <64386953+by-hahn@users.noreply.github.com>
2025-03-13 15:25:57 +0900

    Create Problem_1.cpp

---
+++ 실습 문제/Chapter.01/Problem_1.cpp
@@ -0,0 +1,11 @@
+// (명품) C++ Programming 황기태 저. 생능, 2018
+// ISBN::9788970509433
+
+// 1장 (Chapter.01) 실습문제
+// Problem.1
+
+#include <iostream>
+int main() {
+	std::cout << "My name is Mike. \n";
+	return 0;
+}
