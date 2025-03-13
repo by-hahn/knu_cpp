417d08f20ef958f4c05db95a5ec18add5d5edf94
by-hahn <64386953+by-hahn@users.noreply.github.com>
2025-03-13 15:26:04 +0900

    Create Problem_2.cpp

---
+++ 실습 문제/Chapter.01/Problem_2.cpp
@@ -0,0 +1,11 @@
+// (명품) C++ Programming 황기태 저. 생능, 2018
+// ISBN::9788970509433
+
+// 1장 (Chapter.01) 실습문제
+// Problem.2
+
+#include <iostream>
+int main() {
+	std::cout << "컴퓨터공학과 \n21세 \n대통령";
+	return 0;
+}
