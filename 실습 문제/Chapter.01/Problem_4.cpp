476669bfed21e9fe3ef50f4eb319ddbbfa04f365
by-hahn <64386953+by-hahn@users.noreply.github.com>
2025-03-13 15:26:26 +0900

    Create Problem_4.cpp

---
+++ 실습 문제/Chapter.01/Problem_4.cpp
@@ -0,0 +1,16 @@
+// (명품) C++ Programming 황기태 저. 생능, 2018
+// ISBN::9788970509433
+
+// 1장 (Chapter.01) 실습문제
+// Problem.4
+
+#include <iostream>
+int main() {
+	for (int i = 1; i <= 4; i++) {
+		for (int k = 1; k <= i; k++) {
+			std::cout << "*";
+		}
+		std::cout << "\n";
+	}
+	return 0;
+}
