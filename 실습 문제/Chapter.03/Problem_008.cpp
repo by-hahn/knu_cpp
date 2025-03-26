f942b0c6a3e0af14b415d4e9ceabb4d1caa0f91a
Byoungyoon Hahn <64386953+by-hahn@users.noreply.github.com>
2025-03-26 23:55:00 +0900

    Update Problem_008.cpp

---
+++ 실습 문제/Chapter.03/Problem_008.cpp
@@ -0,0 +1,36 @@
+//Problem 8
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Integer {
+	int a;
+public:
+	Integer(int b) {
+		a = b;
+	}
+	Integer(string s) {
+		a = stoi(s);
+	}
+	int get() {
+		return a;
+	}
+	int set(int b) {
+		return a = b;
+	}
+	int isEven() {
+		return true;
+	}
+};
+
+int main() {
+	Integer n(30);
+	cout << n.get() << ' ';
+	n.set(50);
+	cout << n.get() << ' ';
+	Integer m("300");
+	cout << m.get() << ' ';
+	cout << m.isEven();
+}
+
