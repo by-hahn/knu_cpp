647ac121a569887c07980e01511e9abb40e4c4b3
Byoungyoon Hahn <64386953+by-hahn@users.noreply.github.com>
2025-05-28 23:42:01 +0900

    Update Problem_001.cpp

--- 실습 문제/Chapter.10/Problem_001.cpp
+++ 실습 문제/Chapter.10/Problem_001.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+template <class T>
+T biggest(T arr[], int size) {
+	T big = arr[0];
+	for (int i = 0; i < size; i++) {
+		if (big <= arr[i]) big = arr[i];
+	}
+	return big;
+}
+
+int main() {
+	int x[] = { 1, 10, 100, 5, 4 };
+	cout << biggest(x, 5) << endl;
+}
